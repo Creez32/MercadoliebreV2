93962a3a552551f935aa03160c2f885b856c3cd9
Cristian <cristiannicolas32@hotmail.com>
2021-01-18 18:17:32 -0300

    cambios con maxi

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,9 +2,18 @@ const productos = require('../data/productsDataBase')
 
 module.exports = {
     index:(req,res) =>{
-        
+        const saleProducts = productos.filter((productos)=>{
+            return productos.category === 'in-sale'
+        });
+        const visitedProducts = productos.filter((productos)=>{
+            return productos.category === 'visited'
+        });
+
+        const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+
         res.render('home',{
-            productos
-        })
+            saleProducts,visitedProducts,toThousand
+        });
     }
 }
